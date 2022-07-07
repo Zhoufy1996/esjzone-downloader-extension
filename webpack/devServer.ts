8e6b2cc74e034ed71cc2e7e3d267b4ef2345d206
zhoufeiyu <zhoufeiyu@lianfan.com>
2022-07-07 16:39:56 +0800

    dev 写入磁盘

--- webpack/devServer.ts
+++ webpack/devServer.ts
@@ -3,7 +3,13 @@ import WebpackDevServer from 'webpack-dev-server';
 import devConfig from './webpack.dev';
 
 const compiler = Webpack(devConfig);
-const devServerOptions = { ...devConfig.devServer, open: true };
+const devServerOptions = {
+  ...devConfig.devServer,
+  open: true,
+  devMiddleware: {
+    writeToDisk: true,
+  },
+};
 const server = new WebpackDevServer(devServerOptions, compiler);
 
 const runServer = async () => {
